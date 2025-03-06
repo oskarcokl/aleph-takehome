f58389a8b7f121665a0ac619de2bb6c456184183
oskarcokl <oskar.cokl@gmail.com>
2025-03-06 19:31:24 +0100

    feat: Add basic test for FullInfoView

--- tests/full-info.test.tsx
+++ tests/full-info.test.tsx
@@ -1,11 +1,14 @@
 import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react'
 import React from 'react'
-import FullInfo from '../src/components/full-info'
+import FullInfoComponent from '../src/components/full-info'
+import FullInfoView from '../src/views/full-info'
+import * as booksApi from '../src/api/books';
+import { useParams } from 'react-router-dom';
 
 describe('FullInfo', () => {
   it('renders the FullInfo component', () => {
-    render(<FullInfo coverImageUrl="https://covers.openlibrary.org/b/isbn/9783442236862-L.jpg" title="Snow Crash." authors={["Neal Stephenson"]} publishDate="December 1, 1995" physicalFormat="Paperback" />);
+    render(<FullInfoComponent coverImageUrl="https://covers.openlibrary.org/b/isbn/9783442236862-L.jpg" title="Snow Crash." authors={["Neal Stephenson"]} publishDate="December 1, 1995" physicalFormat="Paperback" />);
     expect(screen.getByText('Snow Crash.')).toBeInTheDocument();
     expect(screen.getByText('Neal Stephenson')).toBeInTheDocument();
     expect(screen.getByText('December 1, 1995')).toBeInTheDocument();
@@ -13,5 +16,9 @@ describe('FullInfo', () => {
 
     const coverImage = screen.getByRole('img');
     expect(coverImage).toHaveAttribute('src', 'https://covers.openlibrary.org/b/isbn/9783442236862-L.jpg');
+  })
+  it('renders the FullInfo view', () => {
+    render(<FullInfoView />);
+    expect(screen.getByText('Full Info')).toBeInTheDocument();
   })
 })
