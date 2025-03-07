54d0ecb764695b0721a9930a6ae7f8463e67b1ae
oskarcokl <oskar.cokl@gmail.com>
2025-03-07 06:15:10 +0100

    feat: Add basic styling for full-info component

--- src/components/full-info.tsx
+++ src/components/full-info.tsx
@@ -6,12 +6,12 @@ export default function FullInfo(props: {
   physicalFormat: string;
 }) {
   return (
-    <div>
-      <img src={props.coverImageUrl} alt="Cover" />
-      <p>Title: {props.title}</p>
-      <p>Authors: {props.authors}</p>
-      <p>Publish Date: {props.publishDate}</p>
-      <p>Physical Format: {props.physicalFormat}</p>
+    <div className="border-2 border-gray-300 rounded-md p-4 w-fit">
+      <img src={props.coverImageUrl} alt="Cover" className="min-h-[450px] min-w-[300px]" />
+      <h4 className="text-lg font-bold">Title: {props.title}</h4>
+      <h5 className="text-md">Authors: {props.authors}</h5>
+      <p className="text-sm">Publish Date: {props.publishDate}</p>
+      <p className="text-sm">Physical Format: {props.physicalFormat}</p>
     </div>
   )
 }
