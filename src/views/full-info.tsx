e0fbedc52304a08a4eb52b5434c996366b50c1e3
oskarcokl <oskar.cokl@gmail.com>
2025-03-07 06:06:44 +0100

    feat: Updated styling for full-info component

--- src/views/full-info.tsx
+++ src/views/full-info.tsx
@@ -8,11 +8,14 @@ export default function FullInfo() {
   const { bookDetails, loading, error } = useBookDetails(isbn!);
 
   return (
-    <section>
-      <h1>Full Info</h1>
-      {loading && <p>Loading...</p>}
-      {bookDetails && !error && <FullInfoComponent coverImageUrl={coverImageUrl} title={bookDetails.title} authors={bookDetails.authors} publishDate={bookDetails.publishDate} physicalFormat={bookDetails.physicalFormat} />}
-      {error && <p>Something went wrong while loading the book details</p>}
+    <section className="mt-10">
+      <h1 className="text-3xl font-bold">Full Info</h1>
+      <a href="/" className="text-blue-500">[Back]</a>
+      <div className="flex flex-col gap-2 mt-4">
+        {loading && <p>Loading...</p>}
+        {bookDetails && !error && <FullInfoComponent coverImageUrl={coverImageUrl} title={bookDetails.title} authors={bookDetails.authors} publishDate={bookDetails.publishDate} physicalFormat={bookDetails.physicalFormat} />}
+        {error && <p>Something went wrong while loading the book details</p>}
+      </div>
     </section>
   );
 }
